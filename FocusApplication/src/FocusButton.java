7a746acea18f15fdcd39d83d17cd2a9404540e41
Zhanxi Ni <ni.zhanxi@gmail.com>
2019-06-03 21:42:53 -0700

    made the timeTrigger method

--- FocusApplication/src/FocusButton.java
+++ FocusApplication/src/FocusButton.java
@@ -1,99 +1,105 @@
 //package components;
  
 import java.awt.*;
+import java.lang.*;
 import java.awt.event.*;
 import javax.swing.*;
  
-class FocusButton extends JFrame implements ActionListener {
+class FocusButton extends JFrame implements WindowListener, ActionListener {
+	
   JButton B; // reference to the button object
   JLabel L; // reference to the label object
-  // constructor for FocusButton
-  
-  public static void main ( String[] args ) {
-	    FocusButton frm = new FocusButton();
+ 
+  boolean on = false; // used for tracking on/off button
 
-	    frm.setSize( 150, 75 );     
-	    frm.setVisible( true );   
+  // constructor for FocusButton
+  public static void main ( String[] args ) {
+	    FocusButton frm = new FocusButton("FOCUS");
+//	    frm.setSize( 400, 400 );     
+//	    frm.setVisible( true );   
+	    
+		//Reset Elapsed time
+	    frm.timeTrigger();
+	    
 	  }
   
-  FocusButton() {
-		L=new JLabel();
-		L.setBounds(50,100,250,20);
-		B=new JButton("Reset Timer");
-		B.setBounds(50,150,95,30);
-		B.addActionListener(this);
+  public FocusButton(String title) {
+	  	
+	  	super(title);
+	  	
+        addWindowListener(this);
 		
-		//JFrame 
+        //JFrame 
 		setSize(400,400);
 		setVisible(true);
+	  	
+	  	//Label
+		L=new JLabel();
+		L.setText("Time is 0 : 0");
+		L.setBounds(50,100,250,20);
+		add(L); 
 		
 		//Button
+		B=new JButton("Reset Timer");
+		B.setBounds(50,150,150,30);
+		add(B);
+		B.addActionListener(this);
+		
+		//timeElapsed
+
   }
   
-//  FocusButton(String title) {
-//    super( title );                     // invoke the JFrame constructor
-//    setLayout( new FlowLayout() );      // set the layout manager
-//
-//    B = new JButton("Start Time!"); // construct a JButton
-//    add( B );                // add the button to the JFrame
-//    setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );   
-//  }
+  long startTime; 
+//  Track the time.
+  public void timeTrigger() {
+	  long time = System.currentTimeMillis();
+	  System.out.println("time = " + time);
+	  
+	  startTime = time; 
+	  long currentTime = System.currentTimeMillis();
+	  long elapsedTime = currentTime - startTime; 
+	  //set the amount of time elapsed
+	  L.setText(String.valueOf(elapsedTime + " milliseconds"));
 
-@Override
-public void actionPerformed(ActionEvent e) {
-	// TODO Auto-generated method stub
-  //Track the time.
-  long startTime = System.currentTimeMillis();
-  long elapsedTime = System.currentTimeMillis() - startTime;
-  long elapsedSeconds = elapsedTime / 1000;
-  long secondsDisplay = elapsedSeconds % 60;
-  long elapsedMinutes = elapsedSeconds / 60;
+	  
+//	  long time = System.currentTimeMillis();
+//	  on = true; 
+//	  if (on) {
+//		  System.out.println("time = " + time);
+//		  long elapsedTime = System.currentTimeMillis() - time;
+//		  L.setText(String.valueOf(elapsedTime));
+//		  on = false; 
+//		  }
+//	  } else {
+//		  
+//	  }
+////	  System.out.println(System.currentTimeMillis()); 
+//	  long elapsedTime = System.currentTimeMillis() - startTime;
+//	  System.out.println(System.currentTimeMillis()); 
+//	  System.out.println("elapsed time = " + elapsedTime);
+//	  long elapsedSeconds = elapsedTime / 1000;
+//	  long secondsDisplay = elapsedSeconds % 60;
+//	  long elapsedMinutes = elapsedSeconds / 60;	
+//	  
+//	  L.setText("Elapsed time is "+ elapsedMinutes +" : "+secondsDisplay);
+
+  }
   
-		L.setText("Elapsed time is "+elapsedMinutes+" : "+secondsDisplay);
-	
+public void actionPerformed(java.awt.event.ActionEvent e) {
+  
+	// the action performed: starts/stops the timer
+	timeTrigger(); 
+	  
 }
+
+
+public void windowOpened(WindowEvent e) {}
+public void windowActivated(WindowEvent e) {}
+public void windowIconified(WindowEvent e) {}
+public void windowDeiconified(WindowEvent e) {}
+public void windowDeactivated(WindowEvent e) {}
+public void windowClosed(WindowEvent e) {}
+public void windowClosing(WindowEvent e) {}
   
 }
-
-
-///* FrameDemo.java requires no other files. */
-//public class main extends JFrame implements ActionListener {
-//	JLabel I; 
-//	JButton b;
-//	
-//	main(){
-//		I=new JLabel();
-//		I.setBounds(50,100,250,20);
-//		b=new JButton("Reset Timer");
-//		b.setBounds(50,150,95,30);
-//		b.addActionListener(this);
-//		
-//		//JFrame 
-//		setSize(400,400);
-//		setVisible(true);
-//		
-//		//Button
-//		
-//	}
-// 
-//    public static void main(String[] args) {
-//    		new main(); 
-//    }
-//
-//	@Override
-//	public void actionPerformed(ActionEvent e) {
-//		// TODO Auto-generated method stub
-//        //Track the time.
-//        long startTime = System.currentTimeMillis();
-//        long elapsedTime = System.currentTimeMillis() - startTime;
-//        long elapsedSeconds = elapsedTime / 1000;
-//        long secondsDisplay = elapsedSeconds % 60;
-//        long elapsedMinutes = elapsedSeconds / 60;
-//        
-//		try {
-//			I.setText("Elapsed time is "+elapsedMinutes+" : "+secondsDisplay);
-//		}catch(Exception ex) {System.out.println(ex);}
-//		}
-//		
-//	}
 
